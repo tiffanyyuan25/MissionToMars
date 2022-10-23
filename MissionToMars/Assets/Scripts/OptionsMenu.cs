b21f350ed6a5df5d6ab3bbdcf8e533ec1e3cb124
Tiffany Yuan <89342012+tiffanyyuan25@users.noreply.github.com>
2022-10-23 13:48:35 +0200

    options menu

--- MissionToMars/Assets/Scripts/OptionsMenu.cs
+++ MissionToMars/Assets/Scripts/OptionsMenu.cs
@@ -2,15 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
+    public Toggle fullscreenTog;
 
     public AudioMixer audioMixer;
 
+    // Graphics component
+    // Start is called before the first frame update
+    public void Start()
+    {
+        fullscreenTog.isOn = Screen.fullScreen;
+    }
+
+
+    // Volume component 
     public void SetVolume (float volume)
     {
         Debug.Log(volume);
         audioMixer.SetFloat("volume", volume);
     }
+
+    public void saveChanges()
+    {
+        Screen.fullScreen = fullscreenTog.isOn;
+    }
+
+
 }
