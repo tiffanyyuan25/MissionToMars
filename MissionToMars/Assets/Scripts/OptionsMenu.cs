090988c06a377f26f0e81e51f63efdde05852161
Tiffany Yuan <89342012+tiffanyyuan25@users.noreply.github.com>
2022-10-23 15:01:32 +0200

    Completed Options Menu

--- MissionToMars/Assets/Scripts/OptionsMenu.cs
+++ MissionToMars/Assets/Scripts/OptionsMenu.cs
@@ -3,18 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using TMPro;
 
 public class OptionsMenu : MonoBehaviour
 {
     public Toggle fullscreenTog;
 
+    public List<ResItem> resolutions = new List<ResItem>();
+    private int selectedResolution;
+    public TMP_Text resolutionLabel; //referencing textmesh pro element 
+
     public AudioMixer audioMixer;
 
-    // Graphics component
+
+    // Graphics components 
     // Start is called before the first frame update
     public void Start()
     {
-        fullscreenTog.isOn = Screen.fullScreen;
+        fullscreenTog.isOn = Screen.fullScreen; //fullscreen
+
+        bool foundRes = false;
+        for (int i = 0; i < resolutions.Count; i++)
+        {
+            if (Screen.width == resolutions[i].horizontal && Screen.height == resolutions[i].vertical)
+            {
+                foundRes = true;
+                selectedResolution = i;
+                UpdateResLabel();
+            }
+        }
+
+        if (!foundRes)
+        {
+            ResItem newRes = new ResItem();
+            newRes.horizontal = Screen.width;
+            newRes.vertical = Screen.height;
+
+            resolutions.Add(newRes);
+            selectedResolution = resolutions.Count - 1;
+
+            UpdateResLabel();
+        }
+    }
+
+    //resolution
+    public void ResLeft()
+    {
+        selectedResolution--;
+        if (selectedResolution < 0)
+        {
+            selectedResolution = 0;
+        }
+        UpdateResLabel();
+    }
+
+    public void ResRight()
+    {
+        selectedResolution++;
+        if (selectedResolution > resolutions.Count - 1)
+        {
+            selectedResolution = resolutions.Count - 1;
+        }
+        UpdateResLabel();
+    }
+
+    public void UpdateResLabel()
+    {
+        resolutionLabel.text = resolutions[selectedResolution].horizontal.ToString() + " x " + resolutions[selectedResolution].vertical.ToString();
     }
 
 
@@ -25,10 +80,19 @@ public class OptionsMenu : MonoBehaviour
         audioMixer.SetFloat("volume", volume);
     }
 
+
+
+    // Save changes on Options Menu 
     public void saveChanges()
     {
-        Screen.fullScreen = fullscreenTog.isOn;
+        //Screen.fullScreen = fullscreenTog.isOn;
+        Screen.SetResolution(resolutions[selectedResolution].horizontal, resolutions[selectedResolution].vertical, fullscreenTog.isOn);
     }
 
+}
 
+[System.Serializable]
+public class ResItem
+{
+    public int horizontal, vertical;
 }
