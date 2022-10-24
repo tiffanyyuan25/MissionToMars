b12d6bff7c7f6daf6a936b64149b06ef1fce0354
Kevaalin <kevaalin99@gmail.com>
2022-10-24 12:58:33 +0200

    Final outer terrain changes

--- MissionToMars/Assets/Scripts/DayClock.cs
+++ MissionToMars/Assets/Scripts/DayClock.cs
@@ -6,12 +6,10 @@ public class DayClock : MonoBehaviour
 {
     public Light lightSource;
     public float xAngle, yAngle, zAngle;
-    public float xPos, yPos, zPos;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("[INFO] Current Position and Orientation for light source: " + lightSource.transform.position + " " + lightSource.transform.localRotation);
-        lightSource.transform.position = new Vector3(xPos, yPos, zPos);
+        Debug.Log("[INFO] Current Position and Orientation for light source: " + lightSource.transform.position + " " + lightSource.transform.localRotation);        
     }
 
     // Update is called once per frame
--- MissionToMars/Assets/Scripts/Resource Cache.cs
+++ MissionToMars/Assets/Scripts/Resource Cache.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResourceCache : MonoBehaviour
+public class ResourceCache : MonoBehaviour, IInteractable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public ResourceItemData ItemData;
+    [SerializeField] private string _prompt;
+    [SerializeField] private int _amountOfOre;
+    [SerializeField] private int _maxYield;
 
-    // Update is called once per frame
-    void Update()
+    private int count = 1;
+    public string InteractionPrompt => _prompt;
+    public int AmountOfOre => _amountOfOre;
+
+    public bool Interact(Interactor interactor)
     {
-        
+        var resource = interactor.ResourceHolder;
+        if (!resource) return false;
+
+        int amountToAdd = _maxYield / count;
+
+        if (amountToAdd > _amountOfOre)
+        {
+            amountToAdd = _amountOfOre;
+        }
+
+        Debug.Log("About to Interact");
+
+        if (resource.ResourceSystem.AddToResources(ItemData, amountToAdd))
+        {
+            _amountOfOre -= amountToAdd;
+            count += 1;
+        }
+
+        return true;
     }
 }
