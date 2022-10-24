c307c24263aa315c6e2a8d033865510c0f69f9d6
Tiffany Yuan <89342012+tiffanyyuan25@users.noreply.github.com>
2022-10-25 00:03:29 +0200

    Added Mission Failed Town Died Ending Scene
    created town died ending scene and made small changes to mission success scene

--- MissionToMars/Assets/Scripts/Ending Scripts/TownDied.cs
+++ MissionToMars/Assets/Scripts/Ending Scripts/TownDied.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MissionSuccessScript : MonoBehaviour
+public class TownDied : MonoBehaviour
 {
     public void LoadMenu()
     {
