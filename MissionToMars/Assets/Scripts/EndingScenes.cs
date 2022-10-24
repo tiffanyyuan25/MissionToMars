cd3d2b591324664dca5844d69deb59c59fb6d15a
Tiffany Yuan <89342012+tiffanyyuan25@users.noreply.github.com>
2022-10-25 00:33:38 +0200

    not enough ore ending scene
    Created mission failed: not enough ore resource scene. Made a general script for menu and quit buttons on each scene screen and edited layout of text on each scene screen.

--- MissionToMars/Assets/Scripts/EndingScenes.cs
+++ MissionToMars/Assets/Scripts/EndingScenes.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TownDied : MonoBehaviour
+public class EndingScenes : MonoBehaviour
 {
     public void LoadMenu()
     {
