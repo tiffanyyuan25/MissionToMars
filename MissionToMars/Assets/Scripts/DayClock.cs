152c3114212cda48b27f83ef619a0034a3c78d7f
Kevaalin <kevaalin99@gmail.com>
2022-10-24 11:49:18 +0200

    Changed to scene + save and load + day clock

--- MissionToMars/Assets/Scripts/DayClock.cs
+++ MissionToMars/Assets/Scripts/DayClock.cs
@@ -6,17 +6,19 @@ public class DayClock : MonoBehaviour
 {
     public Light lightSource;
     public float xAngle, yAngle, zAngle;
+    public float xPos, yPos, zPos;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("[INFO] Current Orientation for light source: " + lightSource);
+        Debug.Log("[INFO] Current Position and Orientation for light source: " + lightSource.transform.position + " " + lightSource.transform.localRotation);
+        lightSource.transform.position = new Vector3(xPos, yPos, zPos);
     }
 
     // Update is called once per frame
     void Update()
     {
         lightSource.transform.Rotate(xAngle, yAngle, zAngle, Space.Self);
-        Debug.Log("[INFO] Directional Light Orientation: " + lightSource.transform.rotation);
+        //Debug.Log("[INFO] Directional Light Orientation: " + lightSource.transform.rotation);
         if(lightSource.transform.localRotation.y == 0.64531){
             Debug.Log("[INFO] Game Over --- Day Ended");
         }
